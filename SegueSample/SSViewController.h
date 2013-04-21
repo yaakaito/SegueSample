46842c0ac8dd71d7e4ff67680f5844d56f6c9251
KAZUMA Ukyo <yaakaito@gmail.com>
2013-04-22 08:27:02 +0900

    Initial Commit

---
+++ SegueSample/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  SegueSample
+//
+//  Created by yaakaito on 2013/04/22.
+//  Copyright (c) 2013年 yaakaito.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
